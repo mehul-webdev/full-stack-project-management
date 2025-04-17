92b6e4dee80c295630089049c8bd7686cf681d5d
mehulwebdev96@gmail.com <mehulchaudhary330@gmail.com>
2025-04-18 01:57:00 +0530

    updated vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // or './' depending on your deployment setup
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
@@ -15,7 +16,6 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
